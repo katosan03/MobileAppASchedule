9609945cdec20a972b3f2816c52091549febf373
katosan03 <221205050@ie.meijo-u.ac.jp>
2023-09-07 22:30:41 +0900

    binding,datastoreの設定，簡単なxml作成

--- app/src/main/java/jp/co/meijou/android/mobileappaschedule/MainActivity2.java
+++ app/src/main/java/jp/co/meijou/android/mobileappaschedule/MainActivity2.java
@@ -6,11 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import jp.co.meijou.android.mobileappaschedule.databinding.ActivityMain2Binding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMain2Binding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        prefDataStore = prefDataStore.getInstance(this);
     }
+
+
 }
