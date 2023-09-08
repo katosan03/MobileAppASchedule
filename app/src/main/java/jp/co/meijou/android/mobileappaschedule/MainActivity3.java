e9d9e11c59748037dd057123a87390b59b22258e
Kaito-Tera <221205105@ccmailg.meijo-u.ac.jp>
2023-09-08 10:57:21 +0900

    Kaito レイアウト作成

--- app/src/main/java/jp/co/meijou/android/mobileappaschedule/MainActivity3.java
+++ app/src/main/java/jp/co/meijou/android/mobileappaschedule/MainActivity3.java
@@ -5,18 +5,14 @@ package jp.co.meijou.android.mobileappaschedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+
+
 import android.os.Bundle;
 
 public class MainActivity3 extends AppCompatActivity {
 
     //private ActivityMain3Binding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //binding = ActivityMain3Binding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
 
 
-    }
 }
