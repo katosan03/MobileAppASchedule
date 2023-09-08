15d86dcb43f2a163b257bf2f31ba3057c05913ad
katosan03 <221205050@ie.meijo-u.ac.jp>
2023-09-08 11:18:52 +0900

    MainActivity3のOncreateを修正

--- app/src/main/java/jp/co/meijou/android/mobileappaschedule/MainActivity3.java
+++ app/src/main/java/jp/co/meijou/android/mobileappaschedule/MainActivity3.java
@@ -9,9 +9,19 @@ import android.app.Activity;
 
 import android.os.Bundle;
 
-public class MainActivity3 extends AppCompatActivity {
+import jp.co.meijou.android.mobileappaschedule.databinding.ActivityMain2Binding;
+import jp.co.meijou.android.mobileappaschedule.databinding.ActivityMain3Binding;
 
+public class MainActivity3 extends AppCompatActivity {
+    private ActivityMain3Binding binding;
     //private ActivityMain3Binding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
 
 
 
